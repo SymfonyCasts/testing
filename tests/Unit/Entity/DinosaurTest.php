ef19b96d75f8c933dc98825428d7267fc0c8cc5b
Jesse Rushlow <jr@rushlow.dev>
2022-03-30 12:12:43 -0400

    cleanup useless tests

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -7,21 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testDinoHasProperties(): void
-    {
-        $dino = new Dinosaur(
-            name: 'Big Eaty',
-            genus: 'Tyrannosaurus',
-            length: 15,
-            enclosure: 'Paddock A'
-        );
-
-        self::assertSame('Big Eaty', $dino->name);
-        self::assertSame('Tyrannosaurus', $dino->genus);
-        self::assertSame(15, $dino->length);
-        self::assertSame('Paddock A', $dino->enclosure);
-    }
-
     public function testDinoOver15MetersIsLarge(): void
     {
         $dino = new Dinosaur(
