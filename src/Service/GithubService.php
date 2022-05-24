5ecc5d7824421df0f7fe2d37d15ea2f5141dc32c
Jesse Rushlow <jr@rushlow.dev>
2022-05-23 20:16:52 -0400

    Step 29: GitHub Service: log exceptions from the http-client

--- src/Service/GithubService.php
+++ src/Service/GithubService.php
@@ -2,15 +2,25 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GithubService
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function getHealthReports(array $dinosaurs): array
     {
         $client = HttpClient::create();
 
-        $response = $client->request('GET', 'https://api.github.com/repos/SymfonyCasts/dino-park/issues');
+        try {
+            $response = $client->request('GET', 'https://api.github.com/repos/SymfonyCasts/dino-park/issues');
+        } catch (TransportExceptionInterface $exception) {
+            $this->logger->info($exception->getMessage());
+        }
 
         foreach ($response->toArray() as $issue) {
             foreach($dinosaurs as $dinosaur) {
