bbf69284288036e11b2b05795ca177c406b49b83
Jesse Rushlow <jr@rushlow.dev>
2022-05-23 20:16:50 -0400

    Step 26: GitHub Service: require the http client

--- src/Service/GithubService.php
+++ src/Service/GithubService.php
@@ -2,13 +2,23 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpClient\HttpClient;
+
 class GithubService
 {
     public function getHealthReports(array $dinosaurs): array
     {
-        // Call Github API
+        $client = HttpClient::create();
 
-        // Filter the issues
+        $response = $client->request('GET', 'https://api.github.com/repos/SymfonyCasts/dino-park/issues');
+
+        foreach ($response->toArray() as $issue) {
+            foreach($dinosaurs as $dinosaur) {
+                if (str_contains($issue['title'], $dinosaur->getName())) {
+                    $dinosaur->setHealth($issue['labels'][0]['name']);
+                }
+            }
+        }
 
         return $dinosaurs;
     }
