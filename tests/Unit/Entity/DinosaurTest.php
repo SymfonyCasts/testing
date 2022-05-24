9c1d8fe49217d991445af683646b703d2dd410bd
Jesse Rushlow <jr@rushlow.dev>
2022-05-23 20:16:40 -0400

    Step 9: TDD: intro TDD with testGetSpecification

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -21,4 +21,16 @@ class DinosaurTest extends TestCase
         self::assertSame(15, $dino->getLength(), 'Dino length getter is not the same as the constructor');
         self::assertSame('Paddock A', $dino->getEnclosure());
     }
+
+    public function testDinoOver15MetersIsLarge(): void
+    {
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A',
+        );
+
+        self::assertSame('Large', $dino->getSpecification(), 'This is supposed to be a large Dinosaur');
+    }
 }
