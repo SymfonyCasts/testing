f9a6daa32958836098a3b72c5394316a2f4d48e3
Jesse Rushlow <jr@rushlow.dev>
2022-07-07 08:19:04 -0400

    optional 3rd & 4th argument

--- src/Entity/Dinosaur.php
+++ src/Entity/Dinosaur.php
@@ -9,7 +9,7 @@ class Dinosaur
     private int $length;
     private string $enclosure;
 
-    public function __construct(string $name, string $genus, int $length, string $enclosure)
+    public function __construct(string $name, string $genus, int $length = 0, string $enclosure = null)
     {
         $this->name = $name;
         $this->genus = $genus;
