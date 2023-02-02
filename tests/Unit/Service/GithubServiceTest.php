f0c1505c82cf2e0c206bc48ec9af1a1b38519140
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:06:48 -0500

    [tuts-apply-diff] Applying existing diff file for step github-service-test-for-getdinohealthreports

---
+++ tests/Unit/Service/GithubServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Unit\Service;
+
+use App\Enum\HealthStatus;
+use PHPUnit\Framework\TestCase;
+
+class GithubServiceTest extends TestCase
+{
+    /**
+     * @dataProvider dinoNameProvider
+     */
+    public function testGetHealthReportReturnsCorrectHealthStatusForDino(HealthStatus $expectedStatus, string $dinoName): void
+    {
+        $service = new GithubService();
+
+        self::assertSame($expectedStatus, $service->getHealthReport($dinoName));
+    }
+
+    public function dinoNameProvider(): \Generator
+    {
+        yield 'Sick Dino' => [
+            HealthStatus::SICK,
+            'Daisy',
+        ];
+
+        yield 'Healthy Dino' => [
+            HealthStatus::HEALTHY,
+            'Maverick',
+        ];
+    }
+}
