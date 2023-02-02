51e7d957e4e102a48e6a55cea6a2a09fb6b3f690
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:06:58 -0500

    [tuts-apply-diff] Applying existing diff file for step exceptions-copy-testdinohealth-and-rename

--- tests/Unit/Service/GithubServiceTest.php
+++ tests/Unit/Service/GithubServiceTest.php
@@ -58,4 +58,36 @@ class GithubServiceTest extends TestCase
             'Maverick',
         ];
     }
+
+    public function testExceptionThrownWithUnknownLabel(string $expectedStatus, string $dinoName): void
+    {
+        $mockLogger = $this->createMock(LoggerInterface::class);
+        $mockHttpClient = $this->createMock(HttpClientInterface::class);
+        $mockResponse = $this->createMock(ResponseInterface::class);
+
+        $mockResponse
+            ->method('toArray')
+            ->willReturn([
+                [
+                    'title' => 'Daisy',
+                    'labels' => [['name' => 'Status: Sick']],
+                ],
+                [
+                    'title' => 'Maverick',
+                    'labels' => [['name' => 'Status: Healthy']],
+                ],
+            ])
+        ;
+
+        $mockHttpClient
+            ->expects(self::once())
+            ->method('request')
+            ->with('GET', 'https://api.github.com/repos/SymfonyCasts/dino-park/issues')
+            ->willReturn($mockResponse)
+        ;
+
+        $service = new GithubService($mockHttpClient, $mockLogger);
+
+        self::assertSame($expectedStatus, $service->getHealthReport($dinoName));
+    }
 }
