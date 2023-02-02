541ea5416de16db0116ce08f780f64d8826850ae
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:06:33 -0500

    [tuts-apply-diff] Applying existing diff file for step first-test-simple-assert-equals

---
+++ tests/Unit/Entity/DinosaurTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        self::assertEquals(42, 42);
+    }
+}
