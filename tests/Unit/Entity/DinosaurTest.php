9d921bba1e510759efa5720b98d197ecd203d1c7
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:06:35 -0500

    [tuts-apply-diff] Applying existing diff file for step dinosaur-real-test-useless

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -2,17 +2,23 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testItWorks(): void
+    public function testCanGetAndSetData(): void
     {
-        self::assertEquals('42', 42);
-    }
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A',
+        );
 
-    public function testItWorksTheSame(): void
-    {
-        self::assertSame('42', 42);
+        self::assertSame('Big Eaty', $dino->getName());
+        self::assertSame('Tyrannosaurus', $dino->getGenus());
+        self::assertSame(15, $dino->getLength());
+        self::assertSame('Paddock A', $dino->getEnclosure());
     }
 }
