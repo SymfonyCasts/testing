f476d4bd7feca20c5ca57d6bba3b0315e1f90369
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:07:00 -0500

    [tuts-apply-diff] Applying existing diff file for step exceptions-add-exception-assertion

--- tests/Unit/Service/GithubServiceTest.php
+++ tests/Unit/Service/GithubServiceTest.php
@@ -84,6 +84,8 @@ class GithubServiceTest extends TestCase
 
         $service = new GithubService($mockHttpClient, $mockLogger);
 
+        $this->expectException(\RuntimeException::class);
+
         $service->getHealthReport('Maverick');
     }
 }
