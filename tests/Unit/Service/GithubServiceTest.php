4e9034383e5e503cd85475919c7d9b3c46d14527
Jesse Rushlow <jr@rushlow.dev>
2023-02-02 15:06:54 -0500

    [tuts-apply-diff] Applying existing diff file for step mock-objects-expect-the-client-to-call-request-once

--- tests/Unit/Service/GithubServiceTest.php
+++ tests/Unit/Service/GithubServiceTest.php
@@ -35,6 +35,7 @@ class GithubServiceTest extends TestCase
         ;
 
         $mockHttpClient
+            ->expects(self::once())
             ->method('request')
             ->willReturn($mockResponse)
         ;
