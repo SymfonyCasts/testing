366f293eb25af238659c051dda23a4ff07e56f78
Ryan Weaver <ryan@symfonycasts.com>
2023-10-10 15:17:58 -0400

    Updating README for database, adding fixtures, querying for dinos

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Dinosaur;
+use App\Repository\DinosaurRepository;
 use App\Service\GithubService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +12,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route(path: '/', name: 'main_controller', methods: ['GET'])]
-    public function index(GithubService $github): Response
+    public function index(GithubService $github, DinosaurRepository $repository): Response
     {
-        $dinos = [
-            new Dinosaur('Daisy', 'Velociraptor', 2, 'Paddock A'),
-            new Dinosaur('Maverick','Pterodactyl', 7, 'Aviary 1'),
-            new Dinosaur('Big Eaty', 'Tyrannosaurus', 15, 'Paddock C'),
-            new Dinosaur('Dennis', 'Dilophosaurus', 6, 'Paddock B'),
-            new Dinosaur('Bumpy', 'Triceratops', 10, 'Paddock B'),
-        ];
+        $dinos = $repository->findAll();
 
         foreach ($dinos as $dino) {
             $dino->setHealth($github->getHealthReport($dino->getName()));
